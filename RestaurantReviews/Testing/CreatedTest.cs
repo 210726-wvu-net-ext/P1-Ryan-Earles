b34b1cb654595bde196b7e9d69fc7862b705671c
Rearles <jazzbuddy6@gmail.com>
2021-09-07 20:26:23 -0600

    Changed name of testing

---
+++ RestaurantReviews/Testing/CreatedTest.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using RestaurantReviews.Domain;
+
+namespace Testing
+{
+    //ALL TESTS THAT CHECK IF CREATING SOMETHING ACTUALLY ADDS IT TO THE DATABASE
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        //this method is called before every test is run
+        }
+
+        [Test]
+        public void RestaurantTest()
+        { 
+           
+         
+        }
+        [Test]
+        public void ReviewTest()
+        {
+        }
+        [Test]
+        public void UserTest()
+        {
+
+        }
+        [Test]
+        public void ReviewJoinTest()
+        {
+        }
+
+    }
+}
